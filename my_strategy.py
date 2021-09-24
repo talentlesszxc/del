4a1febcbff75b5d58c97b12a649d47464937e631
talentlesszxc <74401036+talentlesszxc@users.noreply.github.com>
2021-09-24 18:26:38 +0300

    second ci

--- my_strategy.py
+++ my_strategy.py
@@ -11,23 +11,28 @@ class MyStrategy:
         start_planet_coords = (start_planet.x, start_planet.y)
         quarry_properties = game.building_properties[BuildingType.QUARRY]
         index_distance_list = []
+        list_of_indexes = []
+        list_of_values = []
         for index, planet in enumerate(game.planets):
             list_of_indexes.append(index) # получить список индексов планет
-            list_of_values.append(calc_distance!!!!!!!!!!!!!!!!!! # получить список расстояния до каждой планеты 
-            # дальше dictionary = dict(zip(seq, values))
-            # new_dict = {}
-                for x in sorted_keys:
-                    new_dict[x] = dictionary[x]
+            list_of_values.append(self.calc_distance(start_planet.x,
+            start_planet.y, planet.x, planet.y)) # получить список расстояния до каждой планеты 
+            dictionary = dict(zip(list_of_indexes, list_of_values))
+            sorted_keys = sorted(dictionary, key = dictionary.get)
+            new_dict = {}
+            for x in sorted_keys:
+                new_dict[x] = dictionary[x]
                     # готовый словарь, отсортированный в порядке дальности планет от ближайшей к самой дальней
                     # дальше отправлять челов на эти планеты + чекать что там можно построить и строить
-            index_distance = {};
-            index_distance.update({index:self.calc_distance(start_planet.x,
-            start_planet.y, planet.x, planet.y)})
-            print("INDEX",index)
-            print("PLANET", planet)
-            index_distance_list.append(index_distance)            
-        print(index_distance_list)
-        print(index_distance)
+           # index_distance = {};
+           # index_distance.update({index:self.calc_distance(start_planet.x,
+           # start_planet.y, planet.x, planet.y)})
+           # print("INDEX",index)
+           # print("PLANET", planet)
+           # index_distance_list.append(index_distance)            
+        #prin#t(index_distance_list)
+        print#(index_distance)
+        print(new_dict)
 
         # for planet in game.planets:
         #     print(self.calc_distance(start_planet.x, start_planet.y,
